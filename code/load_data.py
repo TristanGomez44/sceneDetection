500ca80f31e6ae3f01aac01b669685bad3f2cffd
Gomez Tristan <gomez.tristan44@gmail.com>
2019-03-12 20:54:36 +0000

    Read a dataset

--- code/load_data.py
+++ code/load_data.py
@@ -5,56 +5,69 @@ import xml.etree.ElementTree as ET
 import os
 import sys
 import cv2
+import numpy as np
 def main():
-    print("Loading data")
 
-    videoPath = "../data/big_buck_bunny_480p_surround-fix.avi"
+    datasetPath = "../data/test.csv"
 
-    if not os.path.exists("../data/result.xml"):
+    videoPaths = np.genfromtxt(datasetPath,dtype=str)
+    #print(videoPaths)
+    for videoPath in videoPaths:
 
-        output = subprocess.Popen(("shotdetect",
-                            "-i",
-                            videoPath,
-                            "-s",
-                            str(60),
-                            "-o",
-                            os.path.dirname(videoPath),
-                            "-f","-l","-m"),
-                            stdout=subprocess.PIPE,
-                            stderr=subprocess.STDOUT)
+        print(videoPath)
 
-        output = output.stdout.read()
+        dirname = "../data/{}/".format(os.path.splitext(os.path.basename(videoPath))[0])
 
+        if not os.path.exists(dirname):
+            os.makedirs(dirname)
 
-    tree = ET.parse("../data/result.xml").getroot()
-    shots = tree.find("content").find("body").find("shots")
+        #Shot extraction
+        if not os.path.exists("{}/result.xml".format(dirname)):
 
-    frameNb = int(shots[-1].get("fduration"))+int(shots[-1].get("fbegin"))
+            print("\t Shot detection")
+            output = subprocess.Popen(("shotdetect",
+                                "-i",
+                                videoPath,
+                                "-s",
+                                str(60),
+                                "-o",
+                                dirname,
+                                "-f","-l","-m"),
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.STDOUT)
 
-    bounds = list(map(lambda x:int(x.get("fbegin")),shots))
-    bounds.append(frameNb)
-    keyFrameInd = []
+            output = output.stdout.read()
 
-    for i,shot in enumerate(bounds):
-        if i < len(bounds)-1:
-            keyFrameInd.append((bounds[i]+bounds[i+1])//2)
+        #Middle frame index extration
+        print("\t Middle frame extraction")
+        tree = ET.parse("../data/{}/result.xml".format(dirname)).getroot()
+        shots = tree.find("content").find("body").find("shots")
 
-    #Writing the middle frame of each shot in a folder
-    dirname = "../data/middleFrames/{}".format(os.path.basename(videoPath).replace(".avi",""))
-    if not os.path.exists(dirname):
+        frameNb = int(shots[-1].get("fduration"))+int(shots[-1].get("fbegin"))
 
-        os.makedirs(dirname)
+        bounds = list(map(lambda x:int(x.get("fbegin")),shots))
+        bounds.append(frameNb)
+        keyFrameInd = []
 
-        cap = cv2.VideoCapture(videoPath)
-        success = True
-        i = 0
-        while success:
-            success, imageRaw = cap.read()
+        for i,shot in enumerate(bounds):
+            if i < len(bounds)-1:
+                keyFrameInd.append((bounds[i]+bounds[i+1])//2)
 
-            if i in keyFrameInd:
-                cv2.imwrite(dirname+"/frame"+str(i)+".png",imageRaw)
+        #Middle frame extraction
+        if not os.path.exists(dirname+"/middleFrames/"):
 
-            i += 1
+            os.makedirs(dirname+"/middleFrames/")
+
+            cap = cv2.VideoCapture(videoPath)
+            success = True
+            i = 0
+            while success:
+                success, imageRaw = cap.read()
+
+                if i in keyFrameInd:
+                    cv2.imwrite(dirname+"/middleFrames/frame"+str(i)+".png",imageRaw)
+
+                i += 1
 
 
 if __name__ == "__main__":
