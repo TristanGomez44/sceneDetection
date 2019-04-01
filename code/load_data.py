519da7e3510767e6171c55df65ddbea9b59c668d
Gomez Tristan <gomez.tristan44@gmail.com>
2019-04-01 18:33:44 +0000

    Turned middle frame extraction into a function

--- code/load_data.py
+++ code/load_data.py
@@ -6,57 +6,37 @@ import os
 import sys
 import cv2
 import numpy as np
-def main():
+import glob
 
-    datasetPath = "../data/test.csv"
+def getMiddleFrames(dataset):
+    #Get the middles frames of each shot for all video in a dataset
+    #Require the shot to be pre-computed.
 
-    videoPaths = np.genfromtxt(datasetPath,dtype=str)
-    #print(videoPaths)
-    for videoPath in videoPaths:
+    videoPathLists = glob.glob("../data/{}/*.*".format(dataset))
 
-        print(videoPath)
-
-        dirname = "../data/{}/".format(os.path.splitext(os.path.basename(videoPath))[0])
-
-        if not os.path.exists(dirname):
-            os.makedirs(dirname)
-
-        #Shot extraction
-        if not os.path.exists("{}/result.xml".format(dirname)):
-
-            print("\t Shot detection")
-            output = subprocess.Popen(("shotdetect",
-                                "-i",
-                                videoPath,
-                                "-s",
-                                str(60),
-                                "-o",
-                                dirname,
-                                "-f","-l","-m"),
-                                stdout=subprocess.PIPE,
-                                stderr=subprocess.STDOUT)
-
-            output = output.stdout.read()
-
-        #Middle frame index extration
-        print("\t Middle frame extraction")
-        tree = ET.parse("../data/{}/result.xml".format(dirname)).getroot()
-        shots = tree.find("content").find("body").find("shots")
-
-        frameNb = int(shots[-1].get("fduration"))+int(shots[-1].get("fbegin"))
-
-        bounds = list(map(lambda x:int(x.get("fbegin")),shots))
-        bounds.append(frameNb)
-        keyFrameInd = []
-
-        for i,shot in enumerate(bounds):
-            if i < len(bounds)-1:
-                keyFrameInd.append((bounds[i]+bounds[i+1])//2)
+    for videoPath in videoPathLists:
 
         #Middle frame extraction
         if not os.path.exists(dirname+"/middleFrames/"):
+            print(videoPath)
 
+            dirname = "../data/{}/{}/".format(dataset,os.path.splitext(os.path.basename(videoPath))[0])
             os.makedirs(dirname+"/middleFrames/")
+
+            #Middle frame index extration
+            print("\t Middle frame extraction")
+            tree = ET.parse("{}/result.xml".format(dirname)).getroot()
+            shots = tree.find("content").find("body").find("shots")
+
+            frameNb = int(shots[-1].get("fduration"))+int(shots[-1].get("fbegin"))
+
+            bounds = list(map(lambda x:int(x.get("fbegin")),shots))
+            bounds.append(frameNb)
+            keyFrameInd = []
+
+            for i,shot in enumerate(bounds):
+                if i < len(bounds)-1:
+                    keyFrameInd.append((bounds[i]+bounds[i+1])//2)
 
             cap = cv2.VideoCapture(videoPath)
             success = True
@@ -68,7 +48,6 @@ def main():
                     cv2.imwrite(dirname+"/middleFrames/frame"+str(i)+".png",imageRaw)
 
                 i += 1
-
 
 if __name__ == "__main__":
     main()
