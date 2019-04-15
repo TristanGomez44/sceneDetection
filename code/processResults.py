f9868d9bf8102bd9df0e7c25783b41feb3ba7061
TristanGomez44 <E144069X@etu.univ-nantes.fr>
2019-04-15 19:03:39 +0200

    Added t-sne plot and a function to compute the metrics

--- code/processResults.py
+++ code/processResults.py
@@ -7,6 +7,46 @@ import matplotlib.pyplot as plt
 plt.switch_backend('agg')
 import sys
 import xml.etree.ElementTree as ET
+from sklearn.manifold import TSNE
+import matplotlib.cm as cm
+def tsne(dataset,exp_id,model_id,seed,nb_scenes=10):
+
+    repFile = glob.glob("../results/{}/")
+
+    videoPathList = glob.glob("../data/{}/*.*".format(dataset))
+    videoPathList = list(filter(lambda x:x.find(".wav")==-1,videoPathList))
+
+    np.random.seed(seed)
+
+    cmap = cm.rainbow(np.linspace(0, 1, nb_scenes))
+
+    for videoPath in videoPathList:
+        videoName = os.path.splitext(os.path.basename(videoPath))[0]
+        print(videoName)
+        if os.path.exists("../results/{}/{}/".format(exp_id,videoName)):
+            print("\tFeature for video {} exist".format(videoName))
+            if not os.path.exists("../vis/{}/{}_model{}_tsne.png".format(exp_id,videoName,model_id)):
+
+                print("\tComputing t-sne")
+                repFilePaths = sorted(glob.glob("../results/{}/{}/*_{}.csv".format(exp_id,videoName,model_id)))
+
+                gt = np.genfromtxt("../data/{}/annotations/{}_targ.csv".format(dataset,videoName)).astype(int)
+                cmap = cm.rainbow(np.linspace(0, 1, gt.sum()+1))
+
+                reps = np.array(list(map(lambda x:np.genfromtxt(x),repFilePaths)))
+
+                gt = gt[:len(reps)]
+
+                repr_tsne = TSNE(n_components=2,init='pca',random_state=1,learning_rate=20).fit_transform(reps)
+
+                plt.figure()
+                plt.title("T-SNE view of feature from {}".format(videoName))
+                plt.scatter(repr_tsne[:,0],repr_tsne[:,1], zorder=2,color=cmap[np.cumsum(gt)])
+                plt.savefig("../vis/{}/{}_model{}_tsne.png".format(exp_id,videoName,model_id))
+            else:
+                print("\tT-sne already done")
+        else:
+            print("\tFeature for video does not {} exist".format(videoName))
 def plotSceneBounds(simMatPath, modelCutPath,outPath):
 
     sceneCuts = np.genfromtxt(modelCutPath)
@@ -83,13 +123,57 @@ def comptGT_trueBaseline(truebaseFold,exp_id,dataset,resPath,frame):
 
         f_score = 2*cov*(1-over)/(cov+(1-over))
 
-        resCSV += vidName+","+str(f_score)+"\n"
+        resCSV +=vidName+","+str(cov)+","+str(over)+","+str(f_score)+"\n"
         print(vidName+","+str(cov)+","+str(over)+","+str(f_score))
-
 
     with open(resPath,"w") as text_file:
         print(resCSV,file=text_file)
 
+def binaryToMetrics(pred,target,seqLen):
+
+    predBounds = []
+    targBounds = []
+
+    for i in range(len(pred)):
+
+        pred_bounds = binaryToSceneBounds(pred[i][:seqLen[i]])
+        targ_bounds = binaryToSceneBounds(target[i][:seqLen[i]])
+
+        print("pred",pred_bounds)
+        print("targ",targ_bounds)
+
+        if len(targ_bounds) > 1:
+            predBounds.append(pred_bounds)
+            targBounds.append(targ_bounds)
+
+    cov_val,overflow_val = 0,0
+    for pred,targ in zip(predBounds,targBounds):
+
+        cov_sam = coverage(np.array(targ),np.array(pred))
+        overf_sam = overflow(np.array(targ),np.array(pred))
+
+        cov_val += cov_sam
+        overflow_val += overf_sam
+
+    cov_val /= len(targBounds)
+    overflow_val /= len(targBounds)
+
+    return cov_val,overflow_val
+
+def binaryToSceneBounds(scenesBinary):
+
+    sceneBounds = []
+    currSceneStart=0
+
+    for i in range(len(scenesBinary)):
+
+        if scenesBinary[i]:
+            sceneBounds.append([currSceneStart,i-1])
+            currSceneStart = i
+
+    sceneBounds.append([currSceneStart,len(scenesBinary)-1])
+
+    return sceneBounds
 
 def frame_to_shots(dataset,vidName,scenesF):
     ''' Computes scene boundaries file with shot index instead of frame index '''
@@ -158,14 +242,14 @@ def coverage(gt,pred):
 
         cov_pred_array = np.zeros(len(pred))
         for j,scene_pred in enumerate(pred):
+
             cov_pred_array[j] = inter(scene,scene_pred)/leng(scene)
         cov_gt_array[i] = cov_pred_array.max()
-
-        cov_gt_array[i] *= leng(scene)/(gt[-1,1])
+        #cov_gt_array[i] *= leng(scene)/(gt[-1,1]+1)
 
     #print(cov_gt_array)
-    return cov_gt_array.sum()
-    #return cov_gt_array.mean()
+    #return cov_gt_array.sum()
+    return cov_gt_array.mean()
 
 def leng(scene):
 
@@ -197,15 +281,16 @@ def overflow(gt,pred):
         elif i == 0:
             ov_gt_array[i] = min(ov_pred_array.sum()/(leng(gt[i+1])),1)
         elif i == len(gt)-1:
+
             ov_gt_array[i] = min(ov_pred_array.sum()/(leng(gt[i-1])),1)
 
-        ov_gt_array[i] *= leng(scene)/(gt[-1,1])
+        #ov_gt_array[i] *= leng(scene)/(gt[-1,1]+1)
 
         #print(leng(scene),gt[-1,1],leng(gt[0]),leng(gt[-1]))
 
     #print(ov_gt_array)
-    return ov_gt_array.sum()
-    #return ov_gt_array[1:-1].mean()
+    #return ov_gt_array.sum()
+    return ov_gt_array.mean()
 
 def IoU(gt,pred):
 
@@ -273,6 +358,9 @@ def main(argv=None):
 
     argreader.parser.add_argument('--metric',type=str,default="IoU",metavar='METRIC',help='The metric to use. Can only be \'IoU\' for now.')
 
+    argreader.parser.add_argument('--tsne',action='store_true',help='To plot t-sne representation of feature extracted. The --exp_id, --model_id, --seed and --dataset_test arguments should\
+                                    be set.')
+
     #Reading the comand line arg
     argreader.getRemainingArgs()
 
@@ -285,8 +373,10 @@ def main(argv=None):
     if args.comp_gt:
         compGT(args.exp_id,args.metric)
     if args.compt_gt_true_baseline:
-        comptGT_trueBaseline(args.compt_gt_true_baseline[0],args.exp_id,args.dataset,args.compt_gt_true_baseline[1],args.frame)
+        comptGT_trueBaseline(args.compt_gt_true_baseline[0],args.exp_id,args.dataset_test,args.compt_gt_true_baseline[1],args.frame)
+    if args.tsne:
 
+        tsne(args.dataset_test,args.exp_id,args.model_id,args.seed)
 
 if __name__ == "__main__":
     main()
