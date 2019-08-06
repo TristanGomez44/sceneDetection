Fixed DED and added ponderation of coverage/overflow by scene length

code/metrics.py
@@ -38,7 +38,7 @@ def binaryToMetrics(pred,target):
 
     return cov_val,overflow_val,iou_val
 
-def binaryToAllMetrics(predBin,targetBin):
+def binaryToAllMetrics(predBin,targetBin,lenPond=True):
     ''' Computes the IoU of a predicted scene segmentation using a gt and a prediction encoded in binary format
 
     This computes IoU relative to prediction and to ground truth and also computes the mean of the two. \
@@ -63,28 +63,31 @@ def binaryToAllMetrics(predBin,targetBin):
             predBounds.append(pred_bounds)
             targBounds.append(targ_bounds)
 
-    iou,iou_pred,iou_gt,over,cover,ded = 0,0,0,0,0,0
+    iou,iou_pred,iou_gt,over,over_new,cover,ded = 0,0,0,0,0,0,0
     for i,(pred,targ) in enumerate(zip(predBounds,targBounds)):
 
         iou_pred += IoU_oneRef(np.array(targ),np.array(pred))
         iou_gt += IoU_oneRef(np.array(pred),np.array(targ))
         iou += iou_pred*0.5+iou_gt*0.5
-        over += overflow(np.array(targ),np.array(pred))
-        cover += coverage(np.array(targ),np.array(pred))
+        over += overflow(np.array(targ),np.array(pred),lenPond)
+        over_new += overflow_new(np.array(targ),np.array(pred),lenPond)
+        cover += coverage(np.array(targ),np.array(pred),lenPond)
         ded += computeDED(targetBin[i].unsqueeze(0),predBin[i].unsqueeze(0))
 
     iou_pred /= len(targBounds)
     iou_gt /= len(targBounds)
     iou /= len(targBounds)
     over /= len(targBounds)
+    over_new /= len(targBounds)
     cover /= len(targBounds)
     ded /= len(targBounds)
 
     f_score = 2*cover*(1-over)/(cover+1-over)
+    f_score_new = 2*cover*(1-over_new)/(cover+1-over_new)
 
-    return {"IoU":iou,"IoU_pred":iou_pred,"IoU_gt":iou_gt,"F-score":f_score,"DED":ded}
+    return {"IoU":iou,"IoU_pred":iou_pred,"IoU_gt":iou_gt,"F-score":f_score,"F-score New":f_score_new,"DED":ded}
 
-def coverage(gt,pred):
+def coverage(gt,pred,lenPond=False):
     ''' Computes the coverage of a scene segmentation
 
     Args:
@@ -104,18 +107,16 @@ def coverage(gt,pred):
 
             cov_pred_array[j] = inter(scene,scene_pred)/leng(scene)
         cov_gt_array[i] = cov_pred_array.max()
-        #cov_gt_array[i] *= leng(scene)/(gt[-1,1]+1)
 
-    #print(cov_gt_array)
-    #return cov_gt_array.sum()
-    return cov_gt_array.mean()
+        if lenPond:
+            cov_gt_array[i] *= leng(scene)/(gt[-1,1]+1)
 
-def leng(scene):
-    ''' The number of shot in an interval, i.e. a scene '''
+    if lenPond:
+        return cov_gt_array.sum()
+    else:
+        return cov_gt_array.mean()
 
-    return scene[1]-scene[0]+1
-
-def overflow(gt,pred):
+def overflow(gt,pred,lenPond=False):
     ''' Computes the overflow of a scene segmentation
 
     Args:
@@ -130,22 +131,10 @@ def overflow(gt,pred):
     ov_gt_array = np.zeros(len(gt))
     for i,scene in enumerate(gt):
 
-
-
-        #if scene[0] == 3 and scene[1] == 21:
-        #    print(scene)
-        #print("Ground truth scene :",i)
-        #print(scene)
         ov_pred_array = np.zeros(len(pred))
         for j,scene_pred in enumerate(pred):
-            #if scene[0] == 3 and scene[1] == 21:
-            #    print(scene_pred,minus(scene_pred,scene),(inter(scene_pred,gt[i-1])>0),(inter(scene_pred,gt[i+1])>0))
-            #ov_pred_array[j] = minus(scene_pred,scene)*(inter(scene_pred,gt[i-1])>0)*(inter(scene_pred,gt[i+1])>0)
-            #ov_pred_array[j] = minus(scene_pred,scene)*(inter(scene_pred,gt[i-1])+inter(scene_pred,gt[i+1])>0)
             ov_pred_array[j] = minus(scene_pred,scene)*min(1,inter(scene_pred,scene))
 
-        #if scene[0] == 3 and scene[1] == 21:
-        #    print("ov_pred_array",ov_pred_array,(leng(gt[i-1])+leng(gt[i+1])))
         if i>0 and i<len(gt)-1:
             ov_gt_array[i] = min(ov_pred_array.sum()/(leng(gt[i-1])+leng(gt[i+1])),1)
         elif i == 0:
@@ -154,13 +143,47 @@ def overflow(gt,pred):
 
             ov_gt_array[i] = min(ov_pred_array.sum()/(leng(gt[i-1])),1)
 
-        #ov_gt_array[i] *= leng(scene)/(gt[-1,1]+1)
+        if lenPond:
+            ov_gt_array[i] *= leng(scene)/(gt[-1,1]+1)
 
-        #print(leng(scene),gt[-1,1],leng(gt[0]),leng(gt[-1]))
+    if lenPond:
+        return ov_gt_array.sum()
+    else:
+        return ov_gt_array.mean()
 
-    #print(ov_gt_array)
-    #return ov_gt_array.sum()
-    return ov_gt_array.mean()
+def overflow_new(gt,pred,lenPond=False):
+    ''' Computes the new overflow of a scene segmentation
+
+    Args:
+    - gt (array): the ground truth segmentation. It is a list of gr interval (each interval is a tuple made of the first and the last shot index of the scene)
+    - pred (array): the predicted segmentation. Same format as gt
+
+    Returns:
+    - the mean overflow of the predicted scene segmentation
+
+    '''
+
+    ov_gt_array = np.zeros(len(gt))
+    for i,scene in enumerate(gt):
+
+        ov_pred_array = np.zeros(len(pred))
+        for j,scene_pred in enumerate(pred):
+            ov_pred_array[j] = leng(scene_pred)*min(1,inter(scene_pred,scene))
+
+        ov_gt_array[i] = 1-leng(scene)/ov_pred_array.sum()
+
+        if lenPond:
+            ov_gt_array[i] *= leng(scene)/(gt[-1,1]+1)
+
+    if lenPond:
+        return ov_gt_array.sum()
+    else:
+        return ov_gt_array.mean()
+def leng(scene):
+    ''' The number of shot in an interval, i.e. a scene '''
+
+    return scene[1]-scene[0]+1
+
 
 def IoU(gt,pred):
     ''' Computes the Intersection over Union of a scene segmentation
@@ -237,12 +260,16 @@ def computeDED(segmA,segmB):
 
         #It is required that segmA is the sequence with the greatest number of scenes
         if segmB[i].max() > segmA[i].max():
-            segmA[i],segmB[i] = segmB[i],segmA[i]
-        else:
-            segmA[i],segmB[i] = segmA[i],segmB[i]
+            segmA[i],segmB[i] = segmB[i].clone(),segmA[i].clone()
+
         occMat = torch.zeros((torch.max(segmB[i])+1,torch.max(segmA[i])+1))
         for j in range(len(segmA[i])):
             occMat[segmB[i][j],segmA[i][j]] += 1
+
+        if occMat.size(0) < occMat.size(1):
+            occMat = torch.cat((occMat,torch.zeros(occMat.size(1)-occMat.size(0),occMat.size(1))),dim=0)
+        elif occMat.size(1) < occMat.size(0):
+            occMat = torch.cat((occMat,torch.zeros(occMat.size(0),occMat.size(0)-occMat.size(1))),dim=0)
 
         costMat = torch.max(occMat)-occMat
 
@@ -253,3 +280,9 @@ def computeDED(segmA,segmB):
         ded += (len(segmB[i])-correctAssignedShots)/len(segmB[i])
 
     return ded/len(segmA)
+
+if __name__ == "__main__":
+    segmA = torch.tensor([0,1,0,0,0,0,1,0,0,0,1,1,0,0])
+    segmB = torch.tensor([0,0,0,0,1,0,0,1,0,1,0,0,0,1])
+
+    computeDED(segmA.unsqueeze(0),segmB.unsqueeze(0))