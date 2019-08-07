40c3d85d419bb152a776a1b1d16f4687f7e5d9c3
TristanGomez44 <tristan.gomez@etu.univ-nantes.fr>
2019-08-07 21:50:38 +0200

    Added scripts to reproduce results

---
+++ code/allRes.sh
@@ -0,0 +1,32 @@
+evalOneDataset () {
+
+  #First arg in the name of the experience (e.g. for an evaluation on the BBC dataset you can call it "evalBBC")
+  #Second arg is the name of the dataset (e.g. "bbc" if you want to run an eval on the BBC dataset)
+
+  python trainVal.py -c model.config --exp_id $1  --train_part_end 0 --dataset_val $2 --val_part_beg 0 \
+                                     --epochs 63  --start_mode fine_tune --init_path ../models/holly2/modellstm_res50_holly2_epoch63   --model_id res50_lstm_Holly2_7
+
+  python trainVal.py -c model.config --exp_id $1  --train_part_end 0 --dataset_val $2 --val_part_beg 0 \
+                                     --epochs 41  --start_mode fine_tune --init_path ../models/holly2/modelres50_res50_holly2_epoch41  --model_id res50_res50_Holly2_7    --temp_model resnet50
+
+  python trainVal.py -c model.config --exp_id $1  --train_part_end 0 --dataset_val $2 --val_part_beg 0 \
+                                     --epochs 144 --start_mode fine_tune --init_path ../models/youtLarg/modelres50_res50_yout_epoch144        --model_id res50_res50_youtLarg_7  --temp_model resnet50
+
+  python trainVal.py -c model.config --exp_id $1  --train_part_end 0 --dataset_val $2 --val_part_beg 0 \
+                                     --epochs 157 --start_mode fine_tune --init_path ../models/youtLarg/modelres101_res50_yout_epoch157       --model_id res50_res101_youtLarg_7 --temp_model resnet50 --feat resnet101
+
+  python trainVal.py -c model.config --exp_id $1  --train_part_end 0 --dataset_val $2 --val_part_beg 0 \
+                                     --epochs 240 --start_mode fine_tune --init_path ../models/youtLarg/modelresnet50_res50_SC_yout_epoch240  --model_id res50_res50_convSco_youtLarg_7  --temp_model resnet50 --score_conv_wind_size 9
+
+  python trainVal.py -c model.config --exp_id $1  --train_part_end 0 --dataset_val $2 --val_part_beg 0 \
+                                     --epochs 151 --start_mode fine_tune --init_path ../models/youtLarg/modelres50_res50_Siam_yout_epoch151   --model_id res50_res50_siam0005_youtLarg_7  --temp_model resnet50
+
+  python trainVal.py -c model.config --exp_id $1  --train_part_end 0 --dataset_val $2 --val_part_beg 0 \
+                                     --epochs 202 --start_mode fine_tune --init_path ../models/youtLarg/modelres50_res50_DT_yout_epoch202     --model_id res50_res50_d1_youtLarg_7  --temp_model resnet50
+
+  python trainVal.py -c model.config --exp_id $1  --train_part_end 0 --dataset_val $2 --val_part_beg 0 \
+                                     --epochs 163 --start_mode fine_tune --init_path ../models/youtLarg/modelres50_res50_adv_yout_epoch163    --model_id res50_res50_advL1_slowDisc_youtLarg_7  --temp_model resnet50
+
+}
+
+evalOneDataset $1 $2
---
+++ code/findBestThres_leaveOneOut.sh
@@ -0,0 +1,16 @@
+rm ../results/$2_metrics.csv
+#mv ../results/$2_metrics.csv ../results/$2_metrics_old.csv
+
+#python processResults.py --exp_id $1 --dataset_test $2 $3 --model_id baseline             --eval_model_leave_one_out 0  0 1 --model_name "Baseline"
+
+python processResults.py --exp_id $1 --dataset_test $2 $3 --model_id res50_lstm_Holly2_7             --eval_model_leave_one_out 63  0 1 --model_name "LSTM-Res50 (Holly2)"
+python processResults.py --exp_id $1 --dataset_test $2 $3 --model_id res50_res50_Holly2_7            --eval_model_leave_one_out 41  0 1 --model_name "Res50-Res50 (Holly2)"
+python processResults.py --exp_id $1 --dataset_test $2 $3 --model_id res50_res50_youtLarg_7          --eval_model_leave_one_out 144 0 1 --model_name "Res50-Res50 (Youtube-large)"
+python processResults.py --exp_id $1 --dataset_test $2 $3 --model_id res50_res101_youtLarg_7         --eval_model_leave_one_out 157 0 1 --model_name "Res50-Res101 (Youtube-large)"
+python processResults.py --exp_id $1 --dataset_test $2 $3 --model_id res50_res50_convSco_youtLarg_7  --eval_model_leave_one_out 240 0 1 --model_name "Res50-Res50 SC (Youtube-large)"
+python processResults.py --exp_id $1 --dataset_test $2 $3 --model_id res50_res50_siam0005_youtLarg_7 --eval_model_leave_one_out 151 0 1 --model_name "Res50-Res50 Siam. (Youtube-large)"
+python processResults.py --exp_id $1 --dataset_test $2 $3 --model_id res50_res50_d1_youtLarg_7       --eval_model_leave_one_out 202 0 1 --model_name "Res50-Res50 DT (Youtube-large)"
+python processResults.py --exp_id $1 --dataset_test $2 $3 --model_id res50_res50_advL1_slowDisc_youtLarg_7       --eval_model_leave_one_out 163 0 1 --model_name "Res50-Res50 Adv. Loss (Youtube-large)"
+
+
+
